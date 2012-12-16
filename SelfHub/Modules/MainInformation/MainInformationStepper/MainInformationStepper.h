e5560e03417a203abd17b29d2a16e70a3baf3ba4
EvgenKor <evgen2k7@yandex.ru>
2012-12-16 22:13:34 +0400

    ProfileModule design. Splash-screen

---
+++ SelfHub/Modules/MainInformation/MainInformationStepper/MainInformationStepper.h
@@ -0,0 +1,13 @@
+//
+//  MainInformationStepper.h
+//  HealthCare
+//
+//  Created by Eugine Korobovsky on 15.12.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainInformationStepper : UISwitch
+
+@end
---
+++ SelfHub/Modules/MainInformation/MainInformationStepper/MainInformationStepper.m
@@ -0,0 +1,31 @@
+//
+//  MainInformationStepper.m
+//  HealthCare
+//
+//  Created by Eugine Korobovsky on 15.12.12.
+//
+//
+
+#import "MainInformationStepper.h"
+
+@implementation MainInformationStepper
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
